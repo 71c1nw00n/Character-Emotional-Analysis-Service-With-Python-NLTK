9d35ae507892a519c913545be78a638983690920
Seungjun Park <sonagi1727@gmail.com>
2024-05-30 17:12:22 +0900

    Update __init__.py
    감정 분석 모듈에 감정 분류 클래스를 Enum으로 선언해주기

--- emotion_analyizer/__init__.py
+++ emotion_analyizer/__init__.py
@@ -1,5 +1,16 @@
 from .text_processor import TextProcessor
 from .emotion_detector import EmotionDetector
 from .character_analyzer import CharacterAnalyzer
+from enum import Enum
 
 __all__ = ["TextProcessor", "EmotionDetector", "CharacterAnalyzer"]
+
+class Emotion(Enum):
+    FEAR = 'fear'
+    ANGER = 'anger'
+    SADNESS = 'sadness'
+    JOY = 'joy'
+    ACCEPTANCE = "acceptance"
+    DISGUST = "disgust"
+    ANTICIPATION = "anticipation"
+    SURPRISE = "surprise"
