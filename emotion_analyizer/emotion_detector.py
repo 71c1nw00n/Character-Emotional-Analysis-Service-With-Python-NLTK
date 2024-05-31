4d341283e10567ec1c4a2433108c3c1c832c5cd2
Seungjun Park <sonagi1727@gmail.com>
2024-06-01 00:08:15 +0900

    Update emotion_detector.py

--- emotion_analyizer/emotion_detector.py
+++ emotion_analyizer/emotion_detector.py
@@ -1,12 +1,12 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
-
+from . import emotion_types
 class EmotionDetector:
     def __init__(self):
         # 감정 분석 모델 초기화 (예: VADER)
         self.analyzer = SentimentIntensityAnalyzer()
 
     def detect_emotion(self, sentence):
-        for word in sentence.split():
-            if word in emotion_types.Emotion:
-                return word
-        return None  # 감정이 발견되지 않으면 None 반환
+        # 여기를 작성해주시면 됩니다.
+        # 문장별로 8가지 감정별로 분석을 다 해보고
+        # 가장 높은 수치의 감정을 반환하거나
+        # Thresholds 이상의 수치가 없으면 None을 반환해주면 됩니다.
