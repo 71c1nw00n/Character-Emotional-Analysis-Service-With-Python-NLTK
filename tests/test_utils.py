a358ea8701685d4d0b4250428a3f08e1a193b46f
Seungjun Park <sonagi1727@gmail.com>
2024-06-03 16:54:20 +0900

    test function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,31 @@
-def compare_emotion_lists(list1, list2):
-    # 두 감정 리스트의 유사도 계산 로직
-    # ... (구현 필요)
-    return similarity_score
+def compare_emotion_lists(answer_dict, output_dict):
+    emotion_types = ['fear', 'anger', 'sadness', 'joy', 'acceptance', 'disgust', 'anticipation', 'surprise']
+    true_positive, false_positive, true_negative, false_negative = 0, 0, 0, 0
+
+    for emotion_type in emotion_types:
+        answer_set = set(answer_dict[emotion_type])
+        output_set = set(output_dict[emotion_type])
+        
+        for offset in answer_set + output_set:
+            if offset in answer_set and offset in output_set:
+                true_positive += 1
+            elif offset in answer_set and offset not in output_set:
+                false_positive += 1
+            elif offset not in answer_set and offset in output_set:
+                false_positive += 1
+            elif offset not in answer_set and offset not in output_set:
+                false_negative += 1
+
+    return true_positive, false_positive, true_negative, false_negative
+
+def precision(tp, fp):
+    return tp / (tp + fp)
+
+def recall(tp, fn):
+    return tp / (tp + fn)
+
+def accuracy(tp, fp, tn, fn):
+    return (tp + tn) / (tp + tn + fp + fn)
+
+def f1(tp, fp, tn, fn):
+    return 2 * precision(tp, fp) * recall(tp, fn) / (precision(tp, fp) + recall(tp, fn))
